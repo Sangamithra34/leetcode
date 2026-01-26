3274ab973c2e1613f29bcd1b0fb30e441256835b
Sangamithra Perumal <perumalsangamithra1310@gmail.com>
2026-01-26 16:32:22 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 1/26/2026, 4:32:51 PM
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 1;
+        int a=0;
+        int b=1;
+        int c=1;
+        for(int i=3;i<=n;i++){
+            int  res=a+b+c;
+            a=b;
+            b=c;
+            c=res;
+        }
+        return c;
+    }
+}
