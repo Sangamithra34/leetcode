658a09d72130ab1505299f4bd918319da71500f9
Sangamithra Perumal <perumalsangamithra1310@gmail.com>
2026-01-26 16:32:06 +0530

    Create 2076-SumOfDigitsOfStringAfterConvert

---
+++ 2076-SumOfDigitsOfStringAfterConvert/2076-SumOfDigitsOfStringAfterConvert.java
@@ -0,0 +1,26 @@
+// Last updated: 1/26/2026, 4:32:35 PM
+class Solution {
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            sb.append(((int)(s.charAt(i)) - 96));
+        }
+        for(int i=0;i<sb.length();i++){
+            sum += ((sb.charAt(i))-'0');
+        }
+        k--;
+        while(k!=0){
+            int num = sum;
+            sum = 0;
+            while(num!=0){
+                int a = num%10;
+                num/=10;
+                sum += a;
+            }
+            k--;
+        }
+        return sum;        
+    }
+}
