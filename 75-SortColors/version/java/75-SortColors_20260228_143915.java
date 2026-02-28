b20649248069ff2c68d9441d452dc34c6c865e92
Sangamithra Perumal <perumalsangamithra1310@gmail.com>
2026-02-28 14:39:15 +0530

    Create 75-SortColors

---
+++ 75-SortColors/version/java/75-SortColors_20260228_143915.java
@@ -0,0 +1,7 @@
+// Last updated: 2/28/2026, 2:39:15 PM
+1class Solution {
+2    public void sortColors(int[] nums) {
+3       Arrays.sort(nums); 
+4       System.out.println(Arrays.toString(nums));
+5    }
+6}
