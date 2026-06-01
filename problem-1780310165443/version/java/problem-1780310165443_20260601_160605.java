9a9dca426d610512d430e744b28bd43f409487c1
Sangamithra Perumal <perumalsangamithra1310@gmail.com>
2026-06-01 16:06:05 +0530

    Create

---
+++ problem-1780310165443/version/java/problem-1780310165443_20260601_160605.java
@@ -0,0 +1,28 @@
+// Last updated: 6/1/2026, 4:06:05 PM
+1class MinStack {
+2    Stack<Long> s = new Stack<>();
+3    long minEle;
+4    public void push(int val) {
+5        if (s.isEmpty()) { 
+6            s.push((long)val); 
+7            minEle = val; 
+8        }
+9        else if (val < minEle) { 
+10            s.push(2L*val - minEle); 
+11            minEle = val; 
+12        }
+13        else s.push((long)val);
+14    }
+15    public void pop() {
+16        long t = s.pop();
+17        if (t < minEle) 
+18            minEle = 2*minEle - t;
+19    }
+20    public int top() {
+21        long t = s.peek();
+22        return t < minEle ? (int)minEle : (int)t;
+23    }
+24    public int getMin() { 
+25        return (int)minEle; 
+26    }
+27}
