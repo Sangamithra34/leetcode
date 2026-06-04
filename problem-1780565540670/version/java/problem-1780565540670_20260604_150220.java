1ca1b0cf8460dc8043bba2bcc59eabdf89fd9a34
Sangamithra Perumal <perumalsangamithra1310@gmail.com>
2026-06-04 15:02:22 +0530

    Create

---
+++ problem-1780565540670/version/java/problem-1780565540670_20260604_150220.java
@@ -0,0 +1,15 @@
+// Last updated: 6/4/2026, 3:02:20 PM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if (n == 0 || n == 1) {
+4            return 1;
+5        }
+6        int a = 1, b = 1;
+7        for (int i = 2; i <= n; i++) {
+8            int temp = b;
+9            b = a + b;
+10            a = temp;
+11        }
+12        return b;
+13    }
+14}
